1d638e24d9e0b072218c71aa205f4d6144a257e3
hip06 <khongpikten21@gmail.com>
2022-04-18 15:27:45 +0700

    deploy nodejs

--- src/models/index.js
+++ src/models/index.js
@@ -9,17 +9,18 @@ const env = process.env.NODE_ENV || 'development';
 const db = {};
 
 let sequelize;
-sequelize = new Sequelize(process.env.DB_DATABASE_NAME, process.env.DB_USERNAME, process.env.DB_PASSWORD, {
-  host: process.env.DB_HOST_NAME,
-  dialect: 'postgres',
-  logging: false,
-  dialectOptions: {
-    ssl: {
-      require: true,
-      rejectUnauthorized: false
+sequelize = new Sequelize(process.env.DB_DATABASE_NAME, process.env.DB_USERNAME, process.env.DB_PASSWORD,
+  {
+    host: process.env.DB_HOST_NAME,
+    dialect: 'postgres',
+    logging: false,
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false
+      }
     }
-  }
-});
+  });
 // if (config.use_env_variable) {
 //   sequelize = new Sequelize(process.env[config.use_env_variable], config);
 // } else {
